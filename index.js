eef3772b51b3bb15a2c1260673773cc14c0cd47d
LingHu878 <70069123+LingHu878@users.noreply.github.com>
2021-06-25 11:41:11 +0530

    first-commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app=express();
 const server=http.createServer(app);
 const io=socketio(server,{
     cors:{
-        origin:["https://messagme.netlify.app/"],
+        origin:["https://messagme.netlify.app"],
         methods: ["GET", "POST"]
         
     }
